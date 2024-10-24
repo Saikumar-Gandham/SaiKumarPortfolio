4c3fd7f54a54348ea9dfa64a14d9e35a5259b120
GandhamSaiKumar <saikumargandham555@gmail.com>
2024-10-23 20:33:31 -0400

    Final1 portfolio design

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('nav');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+const progressLines = document.querySelectorAll('.progress-line span');
+
+window.addEventListener('scroll', () => {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    progressLines.forEach(progress => {
+        const progressTop = progress.getBoundingClientRect().top;
+
+        if (progressTop < triggerBottom) {
+            progress.style.width = progress.parentElement.getAttribute('data-percent');
+        }
     });
- });
- 
- // Typing effect
- const typingText = document.querySelector('.typing-text');
- const words = JSON.parse(typingText.getAttribute('data-words'));
- const wait = typingText.getAttribute('data-wait');
- let index = 0;
- 
- function type() {
-    const currentWord = words[index];
-    typingText.innerHTML = currentWord.substring(0, typingText.innerHTML.length + (typingText.innerHTML.length === currentWord.length ? -currentWord.length : currentWord.length));
-    
-    if (typingText.innerHTML.length === currentWord.length) {
-        setTimeout(() => { index = (index + 1) % words.length; type(); }, wait);
-    } else {
-        setTimeout(type, Math.random() * (300 - 150) + 150);
-    }
- }
- type();
+});
